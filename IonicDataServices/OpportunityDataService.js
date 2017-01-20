4f52e8ceb12ae1bb892db6ce690298d1b138d523
VolunteerNow <kevin.robinson@volunteernow.co.uk>
2017-01-20 21:23:17 +0000

    Added DataServices To Ionic

---
+++ IonicDataServices/OpportunityDataService.js
@@ -0,0 +1,35 @@
+var OpportunityDataService = function($resource, $http, apiBase) {
+	
+    apiBase += "/Opportunity";
+
+    var getAll = function(){
+		return $http.get(apiBase);
+	};
+
+    var getById = function(id) {
+		return $http.get(apiBase + id);
+	};
+    
+    var getAllForCurrentUser = function() {
+        return $http.get(apiBase + '/readAllForCurrentUser');
+    };    
+
+    var modify = function("/opportunity") {
+		if(opportunity._id){
+			return $http.put(apiBase + opportunity._id, opportunity);
+		}
+		return $http.post(apiBase, opportunity);        
+    };
+  
+    var del = function( opportunity) {
+		return $http.delete(apiBase, opportunity);
+	};
+
+    return {
+	    GetAll : getAll,
+	    GetById : getById,
+	    Modify : modify,
+	    Delete : del,
+        GetAllForCurrentUser: getAllForCurrentUser
+    };
+};

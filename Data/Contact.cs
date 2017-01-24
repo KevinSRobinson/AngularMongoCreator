1a1fe0e1215aabf2ed5d9615f8c5325d2ce78bc9
VolunteerNow <kevin.robinson@volunteernow.co.uk>
2017-01-24 11:28:26 +0000

    Added Some more test Tables/ Dev-build running but with blank screen

--- Data/Contact.cs
+++ Data/Contact.cs
@@ -51,7 +51,71 @@ namespace Data
         public string UserId { get; set; }
     }
 
+    public class PlanOfAction
+    {
+        public int OldId { get; set; }
+        public string Achieve { get; set; }
+        public string WhatIDo { get; set; }
+        public string WhoDoIHelp { get; set; }
+        public string UserId { get; set; }
+    }
 
+
+    public class Review
+    {
+        public int OldId { get; set; }
+        public string Comments { get; set; }
+        public string AdminComments { get; set; }
+        public string ReviewDate { get; set; }
+        public string ReviewedBy { get; set; }
+        public string ApprovedOn { get; set; }
+        public decimal HoursAchieved { get; set; }
+        public bool IsApproved { get; set; }
+        public string UserId { get; set; }
+
+    }
+
+    public class SelfAssessment
+    {
+        public int OldId { get; set; }
+        public string Analysis { get; set; }
+        public string Description { get; set; }
+        public string Evaluation { get; set; }
+        public string FutureLearning { get; set; }
+        public string UserId { get; set; }
+
+    }
+
+
+
+    public class TrainingSkill
+    {
+        public int OldId { get; set; }
+        public string CourseTitle { get; set; }
+        public string SkillsLearned { get; set; }
+        public string UserId { get; set; }
+
+    }
+
+
+    public class WhereIVolunteer
+    {
+        public int OldId { get; set; }
+        public string Name { get; set; }
+        public string Organisation { get; set; }
+        public string Details { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Town { get; set; }
+        public string Postcode { get; set; }
+        public string Email { get; set; }
+        public string Tel { get; set; }
+        public string MyOrganisation { get; set; }
+        public string WhatMyOrganisationDoes { get; set; }
+        public bool Agreement { get; set; }
+        public string UserId { get; set; }
+
+    }
 
 
 }
